f6bf0021712c1203463fbaad13976252cdad1131
Marcela Porto <marcela.fporto@gmail.com>
2017-05-02 17:20:24 -0700

    Small changes- attempts to recognize copies from other windows

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
-document.addEventListener('copy', function(e){
-    var copied = (window.getSelection().toString())
-    console.log("Inside the thing")
-    "<li>" + copied + "</li>"
-    var data = $("#status");
-    data.append("<li>"+ copied +"</li><br>")
+document.addEventListener('DOMContentLoaded', function () {
+  document.addEventListener('copy', function(e){
+      var copied = (window.getSelection().toString())
+      console.log("Inside the thing")
+      "<li>" + copied + "</li>"
+      var data = $("#status");
+      data.append("<li>"+ copied +"</li><br>")
+  });
 });
