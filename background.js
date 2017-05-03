0f98bc85f84bfe65c6fbfedc425e3d4f8517a049
Marcela Porto <marcela.fporto@gmail.com>
2017-05-02 21:43:46 -0700

    Attempts to pass information from browser to chrome widget

--- background.js
+++ background.js
@@ -1,12 +1,21 @@
+console.log("In the background`")
 chrome.runtime.onMessage.addListener(
+  console.log("In the listener for message")
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
     if (request.greeting == "hello")
-      console.log(chrome.tabs)
+    console.log("The message cam through")
       chrome.tabs.executeScript({code: 'document.body.style.backgroundColor="red"'})
-      sendResponse({farewell: (chrome.tabs)});
+      sendResponse({farewell: ("blb"))});
+  });
+
+
+  chrome.browserAction.onClicked.addListener(function(tabs.Tab) {
+    console.log("In the listener for icon")
+    chrome.tabs.executeScript({code: 'document.body.style.backgroundColor="red"'});
+    //  chrome.tabs.executeScript(null, {file: "popup.js"});
   });
 
   // console.log(tabs.Tab.highlighted)
--- popup.js
+++ popup.js
@@ -1,28 +1,29 @@
 var array = []
+
+
+
+// Code for browser
 document.addEventListener('copy', function(e){
       var copied = (window.getSelection().toString())
       console.log("Inside the thing")
       array.push(copied)
       console.log(array)
+
+      localStorage.yourObject = JSON.stringify(array);
+      var obj = JSON.parse(localStorage.yourObject || "{}");
+      console.log(obj)
       // data.append("<li>"+ copied +"</li><br>")
 });
 
-var data = document.getElementById("#status");
-data.append("<li>"+ array +"</li><br>")
 
+// Code for popup.html
+document.addEventListener("DOMContentLoaded", function() {
+  chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+      data.innerHTML += "<li> "+ obj +"</li><br>";
+  });
+  document.body.style.backgroundColor="green"
 
-// var array = []
-// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-//   console.log(response.farewell);
-//   document.addEventListener('copy', function(e){
-//       var copied = (window.getSelection().toString())
-//       console.log("Inside the thing")
-//       console.log(copied)
-//       array.push(copied)
-//       console.log(array)
-//       // data.append("<li>"+ copied +"</li><br>")
-//   });
-// });
-//
-//       var data = $("#status");
-//       data.append("<li>"+ array +"</li><br>")
+  var data = document.getElementById("status")
+  data.innerHTML += "<li> IS THIS WORKING </li><br>"
+  data.innerHTML += "<li>" + array + "</li><br>"
+});
