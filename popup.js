294d7e84b45e593bfa6fbd24a9c6d97bc4da5fa5
Marcela Porto <marcela.fporto@gmail.com>
2017-05-02 19:12:05 -0700

    Everything the user copies is stored in a temporary array - still needs to send it to popup.html & be persistent

--- popup.js
+++ popup.js
@@ -1,9 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.addEventListener('copy', function(e){
+var array = []
+document.addEventListener('copy', function(e){
       var copied = (window.getSelection().toString())
       console.log("Inside the thing")
-      "<li>" + copied + "</li>"
-      var data = $("#status");
-      data.append("<li>"+ copied +"</li><br>")
-  });
+      array.push(copied)
+      console.log(array)
+      // data.append("<li>"+ copied +"</li><br>")
 });
+
+var data = document.getElementById("#status");
+data.append("<li>"+ array +"</li><br>")
+
+
+// var array = []
+// chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+//   console.log(response.farewell);
+//   document.addEventListener('copy', function(e){
+//       var copied = (window.getSelection().toString())
+//       console.log("Inside the thing")
+//       console.log(copied)
+//       array.push(copied)
+//       console.log(array)
+//       // data.append("<li>"+ copied +"</li><br>")
+//   });
+// });
+//
+//       var data = $("#status");
+//       data.append("<li>"+ array +"</li><br>")
