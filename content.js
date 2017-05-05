dfe565c742cefca235b35a0f2f8123de5281dc9f
Marcela Porto <marcela.fporto@gmail.com>
2017-05-04 20:31:05 -0700

    ACHIEVED MVP - Refactors

--- content.js
+++ content.js
@@ -3,25 +3,7 @@ document.addEventListener('copy', function(e){
       var copied = (window.getSelection().toString())
       console.log("WHAT WAS COPIED", copied)
 
-      // sets unique key value to insert into localStorage
-      localStorage[Date.now()]= JSON.stringify(copied);
-      var obj = parseLocalStorage();
-      console.log("OBJ", obj)
-
-      // calls background function
-      // var otherWindows = chrome.extension.getBackgroundPage();
-      // console.log(otherWindows.backgroundFunction());
-      chrome.runtime.sendMessage({total_elements: obj}, function(response) {
-        debugger
+      chrome.runtime.sendMessage({total_elements: copied}, function(response) {
         console.log("Response",response);
       });
 });
-
-function parseLocalStorage() {
-  var parsedData = []
-  arrayOfKeys = Object.keys(localStorage)
-  for(var i = 0; i < arrayOfKeys.length; i ++){
-    parsedData.push(JSON.parse(localStorage[arrayOfKeys[i]]))
-  }
-  return parsedData
-}
