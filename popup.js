dbe92c3a70ac685a7bc13848fb313ff3ea0700c8
Marcela Porto <marcela.fporto@gmail.com>
2017-05-08 18:10:06 -0700

    Fixes comma split bug

--- popup.js
+++ popup.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
   clearData();
 
-  arrayOfCopies = localStorage["total_elements"].split(",")
+  arrayOfCopies = localStorage["total_elements"].split("⚙")
   populateScript(arrayOfCopies)
 
 });
 
 function populateScript(array) {
   var data = document.getElementById("status")
-  alert(localStorage["total_elements"].split(",").length)
+  // alert(localStorage["total_elements"].split(",").length)
   for(var i = (array.length - 1) ; i > 0; i--){
     data.innerHTML += "<li> "+ array[i] +" </li><br>"
     if (i === (array.length - 15)) { break;}
